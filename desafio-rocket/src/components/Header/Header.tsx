56ebb650b86950ee0216a13f43f2b82ac9cb4f3c
Sophia <sophiamperez@hotmail.com>
2023-11-27 22:38:14 -0300

    header done

--- desafio-rocket/src/components/Header/Header.tsx
+++ desafio-rocket/src/components/Header/Header.tsx
@@ -1,8 +1,11 @@
 import styles from "./Header.module.css";
 
+import rocket from "../../assets/rocket.svg";
+
 export function Header() {
   return (
     <header className={styles.header}>
+      <img src={rocket} alt="Rocket" />
       <strong className={styles.strong}>to</strong>
       <strong className={styles.strongPurple}>do</strong>
     </header>
